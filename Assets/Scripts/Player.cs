691320d12dda76c0055221805d2c8d64e02a2c04
Juan David <juanmartincas@gmail.com>
2020-02-26 16:54:31 -0500

    Modified some fields

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Color color;
+    public Transform hitTransform;
+    private Vector3 HookShotPosition; 
+    private CharacterController chara;
+    public GameObject weapon; 
+    public float speed = 7.5f;
+    public float offset = 0.5f;
+    public LayerMask mask;
+    public LineRenderer line;
+    
+
+    DistanceJoint2D joint; 
+
+    void Start()
+    {
+        chara = GetComponent<CharacterController>();
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(joint.distance > 1f)
+        {
+            joint.distance -= 0.2f;
+        }
+        else
+        {
+            line.enabled = false;
+            joint.enabled = false; 
+        } 
+         HookShot();
+        
+    }
+
+    private void HookShot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 shootDirection;
+            shootDirection = Input.mousePosition;
+            shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+            shootDirection = shootDirection - this.transform.position;
+            shootDirection.z = 0.0f;
+            RaycastHit2D hit = Physics2D.Raycast(weapon.transform.position, shootDirection, Mathf.Infinity);
+            line.enabled = true;
+           
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                line.SetPosition(1, hit.point); 
+                line.SetPosition(0, weapon.transform.position);
+                
+                HookShotPosition = hit.point; // punto donde colisionó el collider
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.gameObject.GetComponent <Rigidbody2D>();
+                joint.connectedAnchor = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                joint.distance = Vector2.Distance(this.transform.position, HookShotPosition);
+            }
+
+           
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+           Debug.Log("Modifying line");
+           line.SetPosition(0, weapon.transform.position);
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            line.SetPosition(1, weapon.transform.position);
+            joint.enabled = false;
+            line.enabled = false; 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Danger")
+        {
+            Debug.Log("Dead"); 
+        }
+
+        if(other.tag == "Coins")
+        {
+            //collect coins
+        }
+    }
+}

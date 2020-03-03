a8b4c579da4732cfd8178faae9caf8abfde0597c
Carlos <charliemike31245@gmail.com>
2020-03-03 18:51:21 -0500

    generacion arreglada

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -16,15 +16,17 @@ public class Generator : MonoBehaviour
     private int prePos = -99999; 
     public int distanceBtwDanger = 0;
     public bool needRndPos = true; // falso = solo hace spawn en el lugar donde está el generador
+    private float originalPos_x;
 
     public ObjectPooler objectPool; 
 
     void Start()
     {
-        objectPool = GetComponent<ObjectPooler>(); 
+        objectPool = GetComponent<ObjectPooler>();
+        originalPos_x = transform.position.x;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         float yDist = generatorPos.position.y - playerPos.position.y;    
@@ -42,22 +44,17 @@ public class Generator : MonoBehaviour
                         rnd = Random.Range(-3, 4);
                       
                     } while (rnd == prePos);
-                }
-                else
-                {
-                    rnd = (int)generatorPos.transform.position.x; 
-                    Debug.Log(this.transform.name + " Púas en posición: " + rnd); 
-                }               
+                }             
                 GameObject newObj = objectPool.GetPooledObject();
-                generatorPos.transform.position = new Vector3(rnd, generatorPos.transform.position.y + distanceBtwDanger, 0f);
+                generatorPos.transform.position = new Vector2(generatorPos.transform.position.x + rnd, generatorPos.transform.position.y + distanceBtwDanger);
                 newObj.transform.position = generatorPos.transform.position;
                 newObj.transform.rotation = generatorPos.transform.rotation;
                 newObj.SetActive(true);
-                prePos = rnd;
+                generatorPos.transform.position = new Vector2(originalPos_x, generatorPos.transform.position.y);
             }
             else
             {
-                generatorPos.transform.position = new Vector3(rnd, generatorPos.transform.position.y + distanceBtwDanger, 0f);
+                generatorPos.transform.position = new Vector2(generatorPos.transform.position.x, generatorPos.transform.position.y + distanceBtwDanger);
             }
            
         }

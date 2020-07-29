d7e059fb514ccc9f019db965484e091ec8dc5239
Juan Martin <juanmartincas@gmail.com>
2020-07-29 16:41:08 -0500

    Impulse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Player : MonoBehaviour {
 
@@ -12,6 +12,9 @@ public class Player : MonoBehaviour {
     public float distanceBetweenObjects = 10.0f;
     public float grappleVel = 8.0f;
     public int maxDistanceGrapple = 10;
+    public float impulseTrh = 1f;
+    public float impulseForce = 2.0f;
+    public float minVelx = 1.0f;
 
     [Header ("Weapon")]
     public GameObject weapon;
@@ -20,16 +23,16 @@ public class Player : MonoBehaviour {
     private Vector2 hitpoint;
     private Vector3 grapplePoint;
     private Vector3 HookShotPosition;
+    private Vector3 startPosition;
     private Vector3 currentGrapplePosition;
+    private Rigidbody2D rb;
 
     private SpringJoint2D joint;
-
-    private bool hasTouched = false; // detectar si está tocando la pantalla
 
     private void Awake () {
         //  joint = GetComponent<SpringJoint2D> ();
         line = GetComponent<LineRenderer> ();
-
+        rb = GetComponent<Rigidbody2D> ();
         line.enabled = false;
     }
     // Update is called once per frame
@@ -38,7 +41,6 @@ public class Player : MonoBehaviour {
         HookShot ();
 
     }
-
     void LateUpdate () {
         DrawRope ();
     }
@@ -52,6 +54,44 @@ public class Player : MonoBehaviour {
             stopGrap ();
         }
 
+#endif
+        #endregion
+
+        #region Android
+#if UNITY_ANDROID
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch (0);
+
+            switch (touch.phase) {
+                case TouchPhase.Began:
+                    Vector3 pos; //touc.position returns a Vector2, so i addapted it as Vector3                
+                    pos = new Vector3 (touch.position.x, touch.position.y, 0f);
+                    startPosition = pos;
+                    startGrap (pos);
+                    break;
+
+                case TouchPhase.Moved: //calculate direction of impulse
+                    Vector3 newPos = new Vector3 (touch.position.x, touch.position.y, 0f);
+                    float swipeValue = startPosition.x - newPos.x;
+                    Vector2 moveDirection = new Vector2 (rb.velocity.x, rb.velocity.y).normalized;
+                    float nwimfrce = 0;
+                    if (rb.velocity.x < minVelx) {
+                        if (Mathf.Abs (swipeValue) > Mathf.Abs (impulseTrh)) {
+                            if (swipeValue > 0.1) nwimfrce = -impulseForce;                        
+                            if (swipeValue < -0.1) nwimfrce = impulseForce;
+                            rb.AddForce (Vector2.right * nwimfrce, ForceMode2D.Impulse);
+                        }
+                    }
+
+                    startPosition = newPos;
+                    break;
+
+                case TouchPhase.Ended:
+                    // releas target
+                    stopGrap ();
+                    break;
+            }
+        }
 #endif
         #endregion
 
@@ -69,21 +109,20 @@ public class Player : MonoBehaviour {
                 grapplePoint = hit.point;
                 joint = this.gameObject.AddComponent<SpringJoint2D> ();
                 joint.autoConfigureConnectedAnchor = false;
-                //joint.anchor = weapon.transform.position; 
-                joint.connectedBody = hit.collider.gameObject.GetComponent <Rigidbody2D>();
-                joint.connectedAnchor = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D> ();
+                joint.connectedAnchor = hit.point - new Vector2 (hit.collider.transform.position.x, hit.collider.transform.position.y);
 
                 joint.distance = distanceBetweenObjects;
                 joint.dampingRatio = jointDampingRatio;
 
                 line.positionCount = 2;
                 currentGrapplePosition = weapon.transform.position;
-                 hit.collider.GetComponent<TargetManager>().GivePoint(); 
+                hit.collider.GetComponent<TargetManager> ().GivePoint ();
             }
 
         } catch (NullReferenceException ex) {
-            Debug.Log("Catched exception"); 
-            return; 
+            Debug.Log ("Catched exception");
+            return;
         }
 
     }
@@ -106,7 +145,6 @@ public class Player : MonoBehaviour {
         Debug.Log ("Triggered");
         if (other.tag == "Danger") {
             Debug.Log ("Dead");
-            hasTouched = false;
             GameManager.Instance.GameOver ();
         }
 

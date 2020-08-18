655f0093c59120f9d55bbef79f4dd1267c467d81
Juan Martin <juanmartincas@gmail.com>
2020-08-18 14:39:59 -0500

    asas

--- Assets/Scripts/MovingTarget.cs
+++ Assets/Scripts/MovingTarget.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingTarget : TargetManager
+public class MovingTarget : MonoBehaviour
 {
     public Transform posX; 
     public Transform negX; 
